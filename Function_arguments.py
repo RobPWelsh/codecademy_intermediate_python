7960dbf63cc19d189c764359d749faf437932954
Rob Welsh <robpwelsh@gmail.com>
2023-02-09 23:21:20 -0500

    Completed The Nile project

--- Function_arguments.py
+++ Function_arguments.py
@@ -166,10 +166,8 @@ from test import test_function
 
 # Define calculate_shipping_cost() here:
 def calculate_shipping_cost(from_coords, to_coords, shipping_type='Overnight'):
-    from_lat = from_coords[0]
-    from_long = from_coords[1]
-    to_lat = to_coords[0]
-    to_long = to_coords[1]
+    from_lat, from_long = from_coords
+    to_lat, to_long = to_coords
 
     distance = get_distance(from_lat, from_long, to_lat, to_long)
     shipping_rate = SHIPPING_PRICES[shipping_type]
@@ -178,20 +176,34 @@ def calculate_shipping_cost(from_coords, to_coords, shipping_type='Overnight'):
 
 
 # Test the function by calling
-# test_function(calculate_shipping_cost)
+test_function(calculate_shipping_cost)
 
 # Define calculate_driver_cost() here
 def calculate_driver_cost(distance, *drivers):
     cheapest_driver = None
     cheapest_driver_price = None
     for driver in drivers:
-        driver_time = distance / driver
+        driver_time = distance / driver.speed
+        price_for_driver = driver_time * driver.salary
+        if cheapest_driver is None:
+            cheapest_driver = driver
+            cheapest_driver_price = price_for_driver
+        elif price_for_driver < cheapest_driver_price:
+            cheapest_driver = driver
+            cheapest_driver_price = price_for_driver
+    return cheapest_driver_price, cheapest_driver
+
 
 # Test the function by calling
-# test_function(calculate_driver_cost)
+test_function(calculate_driver_cost)
 
 # Define calculate_money_made() here
-
+def calculate_money_made(**trips):
+    total_money_made = 0
+    for trip_id, trip in trips.items():
+        trip_revenue = trip.cost - trip.driver.cost
+        total_money_made += trip_revenue
+    return total_money_made
 
 # Test the function by calling
-# test_function(calculate_money_made)
+test_function(calculate_money_made)
